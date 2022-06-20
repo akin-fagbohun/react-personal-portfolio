746e10cc3ac89117283f4dfd6ace1d3a2e62cf36
Akin Fagbohun <a_fagbohun@yahoo.co.uk>
2022-06-20 16:18:04 +0100

    adjusted footer to match blog footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,30 +6,24 @@ export const Footer = () => {
   return (
     <footer>
       <div className="footer-group">
-        <div className="footer-links">
-          <a target="_blank" href="https://github.com/akin-fagbohun" rel="noreferrer">
+        <a target="_blank" href="https://github.com/akin-fagbohun" rel="noreferrer">
+          <div className="footer-links">
             <GithubLogo />
-          </a>
-          <a target="_blank" href="https://github.com/akin-fagbohun" rel="noreferrer">
             GitHub
-          </a>
-        </div>
-        <div className="footer-links">
-          <a target="_blank" href="https://www.linkedin.com/in/akinfagbohun/" rel="noreferrer">
+          </div>
+        </a>
+        <a target="_blank" href="https://www.linkedin.com/in/akinfagbohun/" rel="noreferrer">
+          <div className="footer-links">
             <LinkedInLogo />
-          </a>
-          <a target="_blank" href="https://www.linkedin.com/in/akinfagbohun/" rel="noreferrer">
             LinkedIn
-          </a>
-        </div>
-        <div className="footer-links">
-          <a target="_blank" href="https://northcoders.com/" rel="noreferrer">
+          </div>
+        </a>
+        <a target="_blank" href="https://northcoders.com/" rel="noreferrer">
+          <div className="footer-links">
             <NorthcodersLogo />
-          </a>
-          <a target="_blank" href="https://northcoders.com/" rel="noreferrer">
             Northcoders
-          </a>
-        </div>
+          </div>
+        </a>
         <small className="copyright">© Akin Fagbohun. All rights reserved.</small>
       </div>
     </footer>
